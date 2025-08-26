45c4fab1ba5c00cfd707ef7a4089839d6639620e
DatBoiTheo <theodorhedhman@gmail.com>
2025-08-26 14:07:10 +0200

    main stuff

--- Program.cs
+++ Program.cs
@@ -1,31 +1,41 @@
 ﻿namespace SchoolUnoProject
 {
-    class Card
-    {
-
-    }
-    class Deck
+    public class variables
     {
 
     }
 
     class Game
     {
-
+        
     }
 
-    class Player
+    class player
     {
-
-    }
-
-    class Program
-    {
-        public static void Main()
+        public player()
         {
             
         }
-    
+
     }
-    
+
+    class program
+    {
+        
+        public static void Main()
+        {
+            bool playernumber = false;
+            bool start = false; 
+            if(Console.ReadLine() == "start".ToLower())
+            {
+                start = true;
+            }
+            else
+            {
+                Console.WriteLine("go fuck yourself");
+            }
+            Console.WriteLine("How many players do you want");
+            playernumber = int.TryParse(Console.ReadLine(), out int playercount);
+        }
+    }
 }
