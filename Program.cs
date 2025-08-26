490463126308c69be49879aeea5a2c98813525cc
Dalamix <dramadalamix@gmail.com>
2025-08-26 12:35:43 +0200

    fixed that joint

--- Program.cs
+++ Program.cs
@@ -16,7 +16,18 @@
 
     class Player
     {
+        private Deck deck;
 
+        public Player(Deck deck)
+        {
+            this.deck = deck;
+        }
+
+        public void SelectCard()
+        {
+            //deck.ListCards();
+            
+        }
     }
 
     class Program
