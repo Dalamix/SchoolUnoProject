8a065f1c1155a64cf0acd3a6bd6770c2a134e93a
DatBoiTheo <theodorhedhman@gmail.com>
2025-08-27 00:10:27 +0200

    I be gooooooning I be gooooooooooning I be gooooooooooooooooooooooooooooooning

--- Program.cs
+++ Program.cs
@@ -1,41 +1,159 @@
 ﻿namespace SchoolUnoProject
 {
-    public class variables
+    public class Card
     {
+        private Random random = new Random();
 
-    }
-
-    class Game
-    {
-        
-    }
-
-    class player
-    {
-        public player()
+        private char[] Colors =
         {
-            
+            'R', 'G', 'B', 'Y', 'W'
+        };
+        private string[] Types =
+        {
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "Blk", "Rev", "+4", "Col"
+        };
+
+        public char Color;
+        public string Type;
+        public string Name;
+        public bool? Special;
+
+        // Creating a card object will randomly select color and type
+        public Card()
+        {
+            Type = Types[random.Next(Types.Length)];
+            Special = (!Char.IsNumber(Type[0])) ? true : false;
+            // We cant have a wildcard 6, it needs to be a special (+4 or color switch)
+            if (Special == true && (Type == "+4" || Type == "Col"))
+            {
+                Color = 'W';
+            }
+            else
+            {
+                Color = Colors[random.Next(Colors.Length - 4)];
+            }
+            Name = Color.ToString() + " " + Type;
+        }
+    }
+
+    class Player
+    {
+        public string Name { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
         }
 
+        public static List<Player> CreatePlayers(int count)
+        {
+            List<Player> players = new List<Player>();
+            for (int i = 1; i <= count; i++)
+            {
+                players.Add(new Player($"Player{i}"));
+            }
+            return players;
+        }
+    }
+    class Game
+    {
+        public void PickUpCard(Player player)
+        {
+            Card newCard = new Card();
+            Console.WriteLine($"{player} picked up a card: {newCard.Name}");
+            Console.WriteLine(); // empty line for readability
+        }
+        public void pickup(Player player)
+        {
+            string input = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            if(input == "pickup")
+            {
+                PickUpCard(player);
+            }
+            else
+            {
+                Console.WriteLine("Unknown Command");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    interface UI
+    {
+        //void ShowHand(List<string> hand);
+        //string PickUpCard(List<string> hand);
+        //void ShowAction(string message);
+        int Start();
+    }
+
+    class UserInterface : UI
+    {
+        //public void ShowHand(List<string> hand)
+        //{
+        //    Console.WriteLine("These are your cards: ");
+        //}
+
+        //public string PickUpCard(List<string> hand)
+        //{
+
+        //}
+
+        //public void ShowAction(string message)
+        //{
+
+        //}
+        public int Start()
+        {
+            string input = "";
+            while (input.ToLower() != "start")
+            {
+                Console.Write("Type 'start' to begin the game: ");
+                input = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            int playerCount = 0;
+            while (true)
+            {
+                Console.Write("Enter number of players (2-4): ");
+                string countInput = Console.ReadLine();
+
+                if (int.TryParse(countInput, out playerCount) && playerCount >= 2 && playerCount <= 4)
+                {
+                    break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("Invalid number. Please enter a number between 2 and 4.");
+            }
+            Console.WriteLine($"Game started with {playerCount} players!");
+            return playerCount;
+        }
     }
 
     class program
     {
-        
         public static void Main()
         {
-            bool playernumber = false;
-            bool start = false; 
-            if(Console.ReadLine() == "start".ToLower())
+            UserInterface userinterface = new UserInterface();
+            int playercount = userinterface.Start();
+            if (playercount == 0)
             {
-                start = true;
+                return;
             }
-            else
-            {
-                Console.WriteLine("go fuck yourself");
-            }
-            Console.WriteLine("How many players do you want");
-            playernumber = int.TryParse(Console.ReadLine(), out int playercount);
+            List<Player> players = Player.CreatePlayers(playercount);
         }
     }
 }
+//bool playernumber = false;
+//bool start = false;
+//if (Console.ReadLine() == "start".ToLower())
+//{
+//    start = true;
+//}     
+//else
+//{
+//    Console.WriteLine("go fuck yourself");
+//}
+//Console.WriteLine("How many players do you want");
+//playernumber = int.TryParse(Console.ReadLine(), out int playercount);
