46c69b0e3c8ff86b49a43972f1f6e6f9710c7bf9
northh <northh20@protonmail.com>
2025-08-26 14:04:16 +0200

    Card class base

--- Program.cs
+++ Program.cs
@@ -1,5 +1,25 @@
 ﻿namespace SchoolUnoProject
 {
+    class Card
+    {
+
+    }
+
+    class Deck
+    {
+
+    }
+
+    class Game
+    {
+
+    }
+
+    class Player
+    {
+
+    }
+
     class Program
     {
         public static void Main()
