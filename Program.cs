a59cfea6a8f2297c86494bd1fe719f72e94082a7
northh <northh20@protonmail.com>
2025-09-02 13:36:56 +0200

    Cleaning up program.cs

--- Program.cs
+++ Program.cs
@@ -3,65 +3,6 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace SchoolUnoProject
 {
-    class Deck
-    {
-        public Deck(Deck deck)
-        {
-            
-        }
-
-        public Card[] ListCards()
-        {
-            Card[] cards = new Card[2];
-            cards[0] = new Card();
-            cards[1] = new Card();
-            return new Card[1];
-            //return deck.ListCards();
-        }
-        public void PickUpCard(Player player)
-        {
-            Card newCard = new Card();
-            Console.WriteLine($"{player} picked up a card: {newCard.Name}");
-            Console.WriteLine(); // empty line for readability
-        }
-    }
-    public class Card
-    {
-        private Random random = new Random();
-
-        private char[] Colors =
-        {
-            'R', 'G', 'B', 'Y', 'W'
-        };
-        private string[] Types =
-        {
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "Blk", "Rev", "+4", "Col"
-        };
-
-
-        public char Color;
-        public string Type;
-        public string Name;
-        public bool? Special;
-
-        // Creating a card object will randomly select color and type
-        public Card()
-        {
-            Type = Types[random.Next(Types.Length)];
-            Special = (!Char.IsNumber(Type[0])) ? true : false;
-            // We cant have a wildcard 6, it needs to be a special (+4 or color switch)
-            if (Special == true && (Type == "+4" || Type == "Col"))
-            {
-                Color = 'W';
-            }
-            else
-            {
-                Color = Colors[random.Next(Colors.Length - 4)];
-            }
-            Name = Color.ToString() + " " + Type;
-        }
-    }
-
     class Player
     {
         public string Name { get; set; }
