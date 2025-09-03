489258e9ff055b59941718e9ba71500e1781e237
DatBoiTheo <theodorhedhman@gmail.com>
2025-09-03 09:35:39 +0200

    goone

--- Program.cs
+++ Program.cs
@@ -119,15 +119,3 @@ namespace SchoolUnoProject
         }
     }
 }
-//bool playernumber = false;
-//bool start = false;
-//if (Console.ReadLine() == "start".ToLower())
-//{
-//    start = true;
-//}     
-//else
-//{
-//    Console.WriteLine("go fuck yourself");
-//}
-//Console.WriteLine("How many players do you want");
-//playernumber = int.TryParse(Console.ReadLine(), out int playercount);
