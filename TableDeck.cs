56f3435349f8298ff513c2f67cdde556b6b9c609
Dalamix <dramadalamix@gmail.com>
2025-09-03 10:48:38 +0200

    Restructuring with a new TableDeck.cs class for managing the Card objects that exist. Card got a new constructor where you can enter parameters to create a card that isn't random. TableDeck so far is able to create the Wilds but will have to have extra methods added to be implemented in its own private CreateCards method which is called during its constructor method.

---
+++ TableDeck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolUnoProject
+{
+    internal class TableDeck
+    {
+        private Random random;
+        private List<Card> tableCards;
+        public TableDeck() 
+        {
+            random = new Random();
+            for (int i = 0; i < 108; i++)
+            {
+                tableCards = CreateCards();
+            }
+
+        }
+
+        private List<Card> CreateCards()
+        {
+            List<Card> cards = new List<Card>();
+            cards.AddRange(CreateWilds());
+
+            return cards;
+        }
+
+        private Card[] CreateWilds()
+        {
+            Card[] cards = new Card[8];
+            for (int i = 0; i < 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    cards[i] = new Card('W', "+4", true);
+                }
+                else
+                {
+                    cards[i] = new Card('W', "Col", true);
+                }
+            }
+
+            return cards;
+        }
+
+        public Card RandomCard()
+        {
+            Card randomCard = tableCards[random.Next(0, tableCards.Count)];
+            tableCards.Remove(randomCard);
+            return randomCard;
+        }
+    }
+}
